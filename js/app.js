9003a825161e629cf950662190bebd4af2bc5808
hbarber2 <hbarber295@gmail.com>
2020-12-10 15:40:17 -0500

    Ready for Slack

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const missed = 0;
+const ul = document.getElementById('list');
+let missed = 0;
 const startGame = document.querySelector('.btn__reset');
-const buttons = document.querySelectorAll('button');
+const button = document.getElementsByTagName('button');
+const heart = document.querySelectorAll('.tries img');
+const overlay = document.getElementById('overlay');
 
 startGame.addEventListener('click', () => {
-	const overlay = document.getElementById('overlay');
 	overlay.style.display = 'none';
-	
 });
 
 let phrases = [
@@ -25,34 +26,63 @@ function getRandomPhraseAsArray(arr) {
 	};
 
 
-const randomPhraseArray = getRandomPhraseAsArray(phrases);
+let randomPhraseArray = getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay(arr) {
 	for (let i = 0; i < arr.length; i++) {
-		if(arr[i] !== ' ') {
-			 arr[i].className = 'letter';
-			 }
-		const li = document.createElement('li');
-		phrase.appendChild(li);
+		let li = document.createElement('li');
 		li.textContent = arr[i];
+		ul.appendChild(li);
+		if(arr[i] !== ' ') {
+			 li.className = 'letter';
+		}
 	}
 };
 
-const addToDisplay = addPhraseToDisplay(randomPhraseArray);
+let addToDisplay = addPhraseToDisplay(randomPhraseArray);
 
 
 function checkLetter(btn) {
-	let li = document.querySelectorAll('.letter');
-	const match = [];
+	let li = document.querySelectorAll(".letter");
+	let match = null;
 	for(let i = 0; i < li.length; i++) {
-		if(li[i] === btn) {
-			li.className = 'show';
-			match.push(li[i]);
-		}else {
-			console.log('null');
+		if(li[i].textContent.toLowerCase() === btn.textContent) {
+			li[i].classList.add('show');
+			match = li[i].textContent;
 		}
 	}
 	return match;
 };
 
+qwerty.addEventListener('click', (e) => {
+	if(e.target.tagName === 'BUTTON'){
+			e.target.className = 'chosen';
+		} else if (e.target) {
+			alert('Please click a button.')
+		}
+	let btnCheck = checkLetter(e.target);
+	if (btnCheck === null) {
+		missed += 1;
+		heart[missed - 1].src = "images/lostHeart.png";
+		}
+	checkWin();
+});
+
+
+
+function checkWin() {
+	let letters = document.querySelectorAll('.letter');
+	let show = document.getElementsByClassName('show');
+		if (letters.length === show.length) {
+			overlay.className = 'win';
+			overlay.style.display = 'flex';
+			document.querySelector('h2').textContent = 'You won!';
+		}else if (missed > 4) {
+			overlay.className = 'lose';
+			overlay.style.display = 'flex';
+			document.querySelector('h2').textContent = 'You lost :(';
+	}
+	
+
+};
 
